9646588e58ce3d9dbccfdaf846464265893f8bd3
vankok-collab <vankokhhh@gmail.com>
2026-01-02 04:34:58 +0000

    JARVIS000

--- app.py
+++ app.py
@@ -111,4 +111,5 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, messages)
 
 if __name__ == "__main__":
-    app.run(port=4000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
